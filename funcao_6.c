6cd2e073375236048c6082b532dc7b4dd7075dff
Brunobassmaster <164246302+Brunobassmaster@users.noreply.github.com>
2025-04-28 16:04:35 -0400

    Update and rename funcao_5.c to funcao_6.c

--- funcao_6.c
+++ funcao_6.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*6 - Faça uma função que recebe a idade de uma pessoa em anos, meses e dias
+/*6 - FaÃ§a uma funÃ§Ã£o que recebe a idade de uma pessoa em anos, meses e dias
 e retorna essa idade expressa em dias.*/
 int funcao(int valor)
   {
